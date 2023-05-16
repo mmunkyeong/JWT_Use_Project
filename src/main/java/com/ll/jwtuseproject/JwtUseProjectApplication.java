a223d83bee20c25d3c574579acc48dfdb709cb93
mmunkyeong <jmk4379@naver.com>
2023-05-16 14:42:07 +0900

    settings

---
+++ src/main/java/com/ll/jwtuseproject/JwtUseProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ll.jwtuseproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtUseProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtUseProjectApplication.class, args);
+    }
+
+}
